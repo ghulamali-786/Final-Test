f3289585cbec69fd896bec1e732a30555f7eb0f6
ghulamali-786 <ghulamali31@gmail.com>
2024-09-25 15:29:08 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,7 @@ if (count === null) {
 // Fetch user ID and username from your server
 async function fetchUserData() {
     try {
-        const response = await fetch('https://1dc2-138-199-53-248.ngrok-free.app -> http://localhost:8080 ');
+        const response = await fetch('https://1dc2-138-199-53-248.ngrok-free.app/get_user_data');
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
